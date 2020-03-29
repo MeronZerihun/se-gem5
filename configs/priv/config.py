4e11e173a82581eec61582cfddcb0d763c962419
Lauren Biernacki <lbiernac@umich.edu>
2020-03-29 13:32:30 -0400

    [SE Mode] Adding test config script to try SE mode instead of FS

---
+++ configs/priv/config.py
@@ -0,0 +1,15 @@
+maxinst=3000000000
+L1_cache_miss_latency=2
+L1_cache_access_latency=2
+L1_Icache_size='16kB'
+L1_Dcache_size='64kB'
+
+L2_cache_miss_latency=20
+L2_cache_access_latency=20
+L2_cache_size='256kB'
+
+churn_sleep_cycles=125e6
+clock_rate='2.5GHz'
+
+systemWidth=4
+
