e4b2067f88efc31ed9e5cacc515aa47b29e24f06
Lauren Biernacki <lbiernac@umich.edu>
2020-10-22 14:33:48 -0400

    [Shadow reg] No more moving backward in time! Bug is fixed, temp registers are ignored, although I think this would be fine because it was not a source of the bug.. What's left to do is clean this bby up then I think we are actually done

--- src/emtd/Metadata.cc
+++ src/emtd/Metadata.cc
@@ -302,21 +302,21 @@ uint64_t Metadata::get_reg_update_time_cycles(RegId regIdx, bool is_fp_op){
     //Returns how many cycles ago the last reg update was
     if (is_fp_op){
         if (fp_reg_updates_ticks.find(regIdx.index()) != fp_reg_updates_ticks.end()){
-            int64_t elapsed_ticks = curTick() - fp_reg_updates_ticks[regIdx.index()];
-            int64_t elapsed_cycles = divCeil(elapsed_ticks, clock_period);
+            uint64_t elapsed_ticks = curTick() - fp_reg_updates_ticks[regIdx.index()];
+            if(curTick() < fp_reg_updates_ticks[regIdx.index()]) {DPRINTF(csd, "\t\t WARNING TIME IS GOING BACKWARD:: R%d update time, cur, stored, elapsed ticks :: %d %d %d\n ", regIdx.index(), curTick(), fp_reg_updates_ticks[regIdx.index()], elapsed_ticks);}
+	    //uint64_t elapsed_cycles = divCeil(elapsed_ticks, clock_period);
+	    uint64_t elapsed_cycles_handcalc = (elapsed_ticks + clock_period - 1) / clock_period;
             // DPRINTF(csd, "\t\t GOT FP R%d update time, last ticks :: %d\n ", regIdx.index(), fp_reg_updates_ticks[regIdx.index()]); 
             // DPRINTF(csd, "\t\t GOT FP Reg update time, elapsed cycles :: %d\n ", elapsed_cycles); 
-            return elapsed_cycles;
+            return elapsed_cycles_handcalc;
         }
     }
     else {
         if (int_reg_updates_ticks.find(regIdx.index()) != int_reg_updates_ticks.end()){
             uint64_t elapsed_ticks = curTick() - int_reg_updates_ticks[regIdx.index()];
             if(curTick() < int_reg_updates_ticks[regIdx.index()]) {DPRINTF(csd, "\t\t WARNING TIME IS GOING BACKWARD:: R%d update time, last ticks :: %d\n ", regIdx.index(), elapsed_ticks);}
-            if(elapsed_ticks < 0) {DPRINTF(csd, "\t\t WARNING ELAPSED_TICKS IS NEGATIVE:: R%d update time, last ticks :: %d\n ", regIdx.index(), elapsed_ticks);}
             //int64_t elapsed_cycles = divCeil(elapsed_ticks, clock_period);
             uint64_t elapsed_cycles_handcalc = (elapsed_ticks + clock_period - 1) / clock_period;
-            if(elapsed_cycles_handcalc < 0) {DPRINTF(csd, "\t\t WARNING ELAPSED_TICKS IS NEGATIVE:: R%d update time, last ticks :: %d\n ", regIdx.index(), elapsed_cycles);}
             // DPRINTF(csd, "\t\t GOT INT R%d update time, last ticks :: %d\n ", regIdx.index(), int_reg_updates_ticks[regIdx.index()]); 
             // DPRINTF(csd, "\t\t INT Reg update time, elapsed cycles :: %d\n ", elapsed_cycles); 
             return elapsed_cycles_handcalc;
@@ -330,13 +330,13 @@ uint64_t Metadata::get_reg_update_time_cycles(RegId regIdx, bool is_fp_op){
 void Metadata::record_reg_update(RegId regIdx, bool is_fp_op, bool is_tainted, bool is_load){
     // If insn is not tainted, no delay for store path, shadow register is duplicate
     // If load, delay for store path is modeled by dec injection, so no added delay here (load followed by immediate store)
-    int update_time = (is_load || !is_tainted) ? 1 : curTick();
+    uint64_t update_time = (is_load || !is_tainted) ? 1 : curTick();
 
     if (!regIdx.isZeroReg()){
         if (is_fp_op){
             //Debug prints
             // if(fp_reg_updates_ticks.find(regIdx.index()) != fp_reg_updates_ticks.end()){
-            //     if(update_time != fp_reg_updates_ticks[regIdx.index()]){ DPRINTF(csd, "\t\t SET FP R%d update time, last ticks :: %d\n ", regIdx.index(), update_time); }
+            if(update_time != fp_reg_updates_ticks[regIdx.index()]){ DPRINTF(csd, "\t\t SET FP R%d update time, last ticks :: %d\n ", regIdx.index(), update_time); }
             // } else { DPRINTF(csd, "\t\t INIT FP R%d update time, last ticks :: %d\n ", regIdx.index(), update_time); }
             //End debug prints
             fp_reg_updates_ticks[regIdx.index()] = update_time;
@@ -344,7 +344,7 @@ void Metadata::record_reg_update(RegId regIdx, bool is_fp_op, bool is_tainted, b
         else {
             //Debug prints
             // if(int_reg_updates_ticks.find(regIdx.index()) != int_reg_updates_ticks.end()){
-            //     if(update_time != int_reg_updates_ticks[regIdx.index()]){ DPRINTF(csd, "\t\t SET FP R%d update time, last ticks :: %d\n ", regIdx.index(), int_reg_updates_ticks[regIdx.index()]); }
+            if(update_time != int_reg_updates_ticks[regIdx.index()]){ DPRINTF(csd, "\t\t SET FP R%d update time, last ticks :: %d\n ", regIdx.index(), int_reg_updates_ticks[regIdx.index()]); }
             // } else { DPRINTF(csd, "\t\t INIT FP R%d update time, last ticks :: %d\n ", regIdx.index(), int_reg_updates_ticks[regIdx.index()]); }
             //End debug prints
             int_reg_updates_ticks[regIdx.index()] = update_time;
