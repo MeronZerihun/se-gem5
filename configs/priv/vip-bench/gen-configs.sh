42d990ce3192ddc100e8f893b4d456c3f63e7f37
Lauren Biernacki <lbiernac@umich.edu>
2022-01-12 16:37:01 -0500

    [VIP] Forgot some config files!

--- configs/priv/vip-bench/gen-configs.sh
+++ configs/priv/vip-bench/gen-configs.sh
@@ -1,4 +1,5 @@
-BENCHMARK_DIRS="distinctness filtered-query flood-fill gradient-descent kadane minspan primal-test shortest-path tea-cipher"
+BENCHMARK_DIRS="lda triangle-count"
+#BENCHMARK_DIRS="distinctness filtered-query flood-fill gradient-descent kadane minspan primal-test shortest-path tea-cipher"
 #"bubble-sort edit-distance eulers-number-approx fft-int kepler-calc mersenne nonlinear-nn nr-solver parrando rad-to-degree lda triangle-count mnist-cnn"
 #BENCHMARK_DIRS="string-search bubble-sort edit-distance eulers-number-approx fft-int kepler-calc mersenne mnist-cnn nonlinear-nn nr-solver parrando rad-to-degree scalar-gwas-chi2 tea-cipher vector-gwas-chi2"
 #BENCHMARK_DIRS="string-search bubble-sort"
